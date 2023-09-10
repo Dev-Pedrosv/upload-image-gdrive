5c0999803f04282c7d4abaf7faddb19a6cad0aea
Pedro Silva <pedro_rike@hotmail.com.br>
2023-09-10 16:13:44 -0300

    feat: create service to add in npm

---
+++ README.md
@@ -0,0 +1,44 @@
+How to use:
+Make sure you have a google account, and you should create a project in https://console.developers.google.com/.
+
+Enter your project's console homepage, and go into APIs & auth -> Credentials, then add a credentials for your project.
+
+Create credentials to GDrive API.
+
+Create the folder in gdrive
+
+add the email client as editor
+
+You'll need {
+  FolderId - Id folder create in Gdrive 
+  client_id: Client Id created by Gdrive API,
+  private_key: Private Key created by Gdrive API,
+  client_email: Client Email created by Gdrive API,
+}
+
+How use ? 
+
+```shell
+  npm i upload-image-gdrive
+```
+
+```js import { uploadImages } from "upload-image-gdrive";
+
+/**
+  * @param   {File[]} files - Array of File objects to upload
+  * 
+*/
+
+const nameFunction = async (files) => {
+  const urlImages = await uploadImages({
+    folderId: folderId,
+    client_id: clientId,
+    private_key: privateKey,
+    client_email: clientEmail,  
+    files
+  })
+
+  return urlImages
+}
+
+```
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { api } from "./api/api.js"
+
+/**
+ * Params to upload images to GDrive
+ *
+ * @param   {string} folderId - Folder id to upload images in GDrive
+ * @param   {string} clientId - Client Id, get in json GDrive
+ * @param   {string} privateKey - Private Key, get in json GDrive
+ * @param   {string} clientEmail - Client Email, get in json GDrive
+ * @param   {File[]} files - Array of File objects to upload
+ * @returns {Promise<string[]>} - Array of image URLs or error messages
+ */
+export const uploadImages = async ({ folderId, clientId, privateKey, clientEmail, files }) => {
+  const formattedParams = {
+    folderId: folderId,
+    client_id: clientId,
+    private_key: privateKey,
+    client_email: clientEmail,
+  };
+
+  try {
+    const uploadPromises = files.map(async (file) => {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const { data } = await api.post('/upload-image', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        params: formattedParams,
+      });
+
+      return data;
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+    return imageUrls;
+  } catch (err) {
+    return err;
+  }
+};
